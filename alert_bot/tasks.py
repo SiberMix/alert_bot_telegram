5b8f63ae52becb3a74226221b18a814095bd3b42
SiberMix <off_mi4ka@vk.com>
2024-08-04 17:04:14 +0700

    add integration VK

--- alert_bot/tasks.py
+++ alert_bot/tasks.py
@@ -1,13 +1,22 @@
 import datetime
 import os
 import random
+import requests
+import logging
 
 from asgiref.sync import sync_to_async
-from telegram import Bot, InputMediaPhoto, InputMediaVideo
+from telegram import Bot, InputMediaPhoto
 from joke_bot.models import User, Joke
 from dotenv import load_dotenv
 
 load_dotenv()
+
+VK_API_VERSION = '5.131'
+VK_ACCESS_TOKEN = os.getenv('API_HASH')
+VK_GROUP_ID = os.getenv('SOURCE_CHANNEL')
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def get_user_list():
@@ -22,6 +31,41 @@ async def get_random_joke():
     return random.choice(jokes)
 
 
+def get_random_vk_image():
+    logger.info("Fetching images from VK group...")
+    url = f"https://api.vk.com/method/wall.get?owner_id=-{VK_GROUP_ID}&count=100&access_token={VK_ACCESS_TOKEN}&v={VK_API_VERSION}"
+    try:
+        response = requests.get(url, timeout=30)  # Увеличиваем тайм-аут до 30 секунд
+        response.raise_for_status()
+        response_json = response.json()
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching from VK: {e}")
+        return None
+
+    if 'error' in response_json:
+        logger.error(f"Error fetching from VK: {response_json['error']}")
+        return None
+
+    posts = response_json.get('response', {}).get('items', [])
+    image_urls = []
+
+    for post in posts:
+        if 'attachments' in post:
+            for attachment in post['attachments']:
+                if attachment['type'] == 'photo':
+                    sizes = attachment['photo']['sizes']
+                    image_url = sizes[-1]['url']  # Выбираем изображение наибольшего размера
+                    image_urls.append(image_url)
+
+    if image_urls:
+        chosen_image = random.choice(image_urls)
+        logger.info(f"Chosen image URL: {chosen_image}")
+        return chosen_image
+    else:
+        logger.info("No images found in VK group.")
+        return None
+
+
 async def send_telegram():
     bot_token = os.getenv('BOT_TOKEN')
     chat_id = os.getenv('CHAT_ID')
@@ -29,6 +73,7 @@ async def send_telegram():
     current_day_of_week = datetime.datetime.today().weekday()
     user_list = await get_user_list()
     user_list = random.sample(user_list, len(user_list))
+
     if current_day_of_week in [0, 2]:  # Понедельник или среда
         backend_users = await get_user_list()
         backend_users = random.sample(backend_users, len(backend_users))
@@ -40,8 +85,7 @@ async def send_telegram():
             chosen_user_kitcpa = random.choice(backend_users_for_release)
             while chosen_user_kitcpa == chosen_user:
                 chosen_user_kitcpa = random.choice(backend_users_for_release)
-            message_text += f"\n\nСегодня релиз KITCPA собирает: '{chosen_user_kitcpa}\n"
-
+            message_text += f"\n\nСегодня релиз KITCPA собирает: {chosen_user_kitcpa}\n"
     else:
         joke = await get_random_joke()
 
@@ -49,13 +93,26 @@ async def send_telegram():
             message_text = ("Сегодня нет дейли. \n"
                             "Шутка дня:\n{}").format(joke.text)
         else:
-            random_users = random.sample(user_list, len(user_list))  # Выбираем случайные пользователей из списка
+            random_users = random.sample(user_list, len(user_list))  # Выбираем случайных пользователей из списка
             message_text = "Порядок проведения дейли:\n{}".format("\n".join([user.username for user in random_users]))
             message_text += "\n\n"
 
-    # Отправка сообщения с изображением
-    message_text += "\n\n"
-    message_text += "ССЫЛКА НА ДЕЙЛИ: https://meet.google.com/izk-wghu-sdg"
-    with open('img.png', 'rb') as img:
-        media = InputMediaPhoto(media=img, caption=message_text, parse_mode='HTML')
-        await bot.send_media_group(chat_id=chat_id, media=[media])
+    image_url = get_random_vk_image()
+    if image_url:
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open('img.png', 'wb') as img_file:
+                img_file.write(response.content)
+
+            logger.info("Image successfully downloaded.")
+
+            with open('img.png', 'rb') as img:
+                media = InputMediaPhoto(media=img, caption=message_text, parse_mode='HTML')
+                await bot.send_media_group(chat_id=chat_id, media=[media])
+                logger.info("Message with image sent to Telegram.")
+        else:
+            logger.error(f"Failed to download image. Status code: {response.status_code}")
+            await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='HTML')
+    else:
+        await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='HTML')
+        logger.info("Message without image sent to Telegram.")
